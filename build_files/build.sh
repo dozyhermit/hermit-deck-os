f4081d81ba75d381e742b26440f19ccf8a8203d8
dozyhermit <dozyhermit@noreply.codeberg.org>
2026-04-23 08:05:41 +1000

    Initial build step for Github actions

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
+# this installs/removes a package from fedora repos
 dnf5 install -y tmux 
+
+# this installs/removes a package from flatpak repos
+# flatpak remove
+
+# setup scripts using ujust
+# ujust setup-sunshine
+# ujust setup-decky
 
 # Use a COPR Example:
 #
@@ -21,4 +28,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
