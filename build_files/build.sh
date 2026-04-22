6b1c63351c9c27104acd15c58029c3ce548b0e53
dozyhermit <dozyhermit@noreply.codeberg.org>
2026-04-23 08:59:28 +1000

    Removing default flatpaks, running ujust commands

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,14 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs/removes a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
 
 # this installs/removes a package from flatpak repos
-# flatpak remove
+xargs -r flatpak remove -y --noninteractive app/com.mattjakeman.ExtensionManager/x86_64/stable app/com.github.Matoking.protontricks/x86_64/stable app/com.ranfdev.DistroShelf/x86_64/stable app/com.github.tchx84.Flatseal/x86_64/stable app/io.github.flattool.Warehouse/x86_64/stable app/com.vysp3r.ProtonPlus/x86_64/stable app/org.gnome.Calculator/x86_64/stable app/org.gnome.Calendar/x86_64/stable app/org.gnome.Characters/x86_64/stable app/org.gnome.Contacts/x86_64/stable app/org.gnome.Papers/x86_64/stable app/org.gnome.Loupe/x86_64/stable app/org.gnome.NautilusPreviewer/x86_64/stable app/org.gnome.TextEditor/x86_64/stable app/org.gnome.Weather/x86_64/stable app/org.gnome.baobab/x86_64/stable app/org.gnome.clocks/x86_64/stable app/org.gnome.font-viewer/x86_64/stable app/org.gnome.Showtime/x86_64/stable app/org.altlinux.Tuner/x86_64/stable app/com.github.Matoking.protontricks/x86_64/stable runtime/com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable runtime/com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable runtime/com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/25.08
 
 # setup scripts using ujust
-# ujust setup-sunshine
-# ujust setup-decky
+xargs -r ujust setup-sunshine
+xargs -r ujust setup-decky
 
 # Use a COPR Example:
 #
