dd2d3d3e8a613e9dbe3fca024436d4f744c5ab88
dozyhermit <dozyhermit@noreply.codeberg.org>
2026-04-23 17:51:52 +1000

    Updating readme, removing GNOME extensions entirely, adding isolated uninstall entry for DistroShelf which sometimes fails to uninstall

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
 # hermit-deck-os
 
-![image description](./logo.png)
-
-_source: https://www.flaticon.com/authors/ains_
+<img src="./logo.png" height="250px" width="auto" alt="https://www.flaticon.com/authors/ains"/>
 
 Bazzite is great, but they target such a wide user base that I find it to be quite bloated.
 
-`hermit-deck-os` is a custom `bazzite-deck-gnome` bootc image designed to strip unnecessary (imo) bloat.
+So `hermit-deck-os` is a custom `bazzite-deck-gnome` bootc image designed to strip unnecessary (imo) bloat.
 
 ## Ethos
 
 Minification, simplification. Bazzite does a lot of things right, but also includes a lot of things like:
 
-1. ROM emulations
-2. Handheld device support
+1. ROM emulators
+2. Handheld support
 3. Game launchers
-4. Proton tweaking toolsets
-5. GUI helpers for things like `brew`, `Proton` or `btrfs`
-6. Gnome desktop environment applets and extensions
-7. Android emulation with Waydroid
+4. Proton toolsets
+5. GUIs for things like `brew` or `btrfs`
+6. GNOME desktop environment applets and extensions
+7. Waydroid emulation
 
 ... which I don't use and haven't for a very long time.
 
@@ -32,22 +30,23 @@ That's it.
 
 ## Changes:
 
-### Applications
+### Removals
 
-The applications removed are:
+The removals are as follows:
 
 1. `waydroid`
 2. `lutris`
 3. `hhd`
 4. `rom-properties`
-5. Numerous flatpaks like: `ProtonTricks`, `ProtonPlus` and the many Gnome applets
-6. GUI applications like: `bbrew`, `btrfs-assistant`, `hhd-ui` and `firewall-config`
+5. Flatpaks: `ProtonTricks`, `ProtonPlus` and the many Gnome applets
+6. GUI application helpers for: `brew`, `btrfs-assistant`, `hhd-ui` and `firewall-config`
+7. GNOME extensions and the extension manager
 
 ### ujust
 
 _Reference: https://docs.bazzite.gg/Installing_and_Managing_Software/ujust/_
 
-The following `ujust` commands are run by default:
+The following `ujust` commands are run:
 
 1. `setup-sunshine`
 2. `setup-decky`
--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,20 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs/removes a package from fedora repos
-# dnf5 install -y tmux 
+# this installs/removes packages from fedora repos or flatpak
+# example: dnf5 install -y tmux 
 
 # this removes flatpak packages
 xargs -r flatpak remove -y --noninteractive app/com.github.Matoking.protontricks/x86_64/stable app/com.ranfdev.DistroShelf/x86_64/stable app/com.github.tchx84.Flatseal/x86_64/stable app/io.github.flattool.Warehouse/x86_64/stable app/com.vysp3r.ProtonPlus/x86_64/stable app/org.gnome.Calculator/x86_64/stable app/org.gnome.Calendar/x86_64/stable app/org.gnome.Characters/x86_64/stable app/org.gnome.Contacts/x86_64/stable app/org.gnome.Papers/x86_64/stable app/org.gnome.Loupe/x86_64/stable app/org.gnome.TextEditor/x86_64/stable app/org.gnome.Weather/x86_64/stable app/org.gnome.baobab/x86_64/stable app/org.gnome.clocks/x86_64/stable app/org.gnome.font-viewer/x86_64/stable app/org.gnome.Showtime/x86_64/stable app/org.altlinux.Tuner/x86_64/stable app/com.github.Matoking.protontricks/x86_64/stable runtime/com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable runtime/com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable runtime/com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/25.08 app/io.missioncenter.MissionCenter/x86_64/stable
+
+# there's a bug with DistroShelf in that it may error when trying to uninstall, so i'm keeping it out of the above flatpak removal and attempting uninstall twice just in case
+xargs -r flatpak remove -y --noninteractive app/com.ranfdev.DistroShelf/x86_64/stable
+xargs -r flatpak remove -y --noninteractive app/com.ranfdev.DistroShelf/x86_64/stable
+
+## this removes the gnome extension manager
+xargs -r gsettings set org.gnome.shell disable-user-extensions true
+xargs -r flatpack remove -y --noninteractive com.mattjakeman.ExtensionManager
+rm -rf /usr/share/gnome-shell/extensions ~/.local/share/gnome-shell/extensions
 
 # this removes dnf packages
 dnf5 remove -y hhd hhd-ui webapp-manager firewall-config btrfs-assistant lutris rom-properties
