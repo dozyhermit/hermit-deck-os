c843ac20ac93316409ae2b35b52bfd66b2aad1a6
dozyhermit <dozyhermit@noreply.codeberg.org>
2026-05-05 17:46:53 +1000

    Removing xargs usage, restructuring build.sh, adding double uninstall for Distroshelf

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,11 +13,19 @@ set -ouex pipefail
 # example: dnf5 install -y tmux 
 
 # this removes flatpak packages
-xargs -r flatpak remove -y --noninteractive app/com.github.Matoking.protontricks/x86_64/stable app/com.github.tchx84.Flatseal/x86_64/stable app/io.github.flattool.Warehouse/x86_64/stable app/com.vysp3r.ProtonPlus/x86_64/stable app/org.gnome.Calculator/x86_64/stable app/org.gnome.Calendar/x86_64/stable app/org.gnome.Characters/x86_64/stable app/org.gnome.Contacts/x86_64/stable app/org.gnome.Papers/x86_64/stable app/org.gnome.Loupe/x86_64/stable app/org.gnome.TextEditor/x86_64/stable app/org.gnome.Weather/x86_64/stable app/org.gnome.baobab/x86_64/stable app/org.gnome.clocks/x86_64/stable app/org.gnome.font-viewer/x86_64/stable app/org.gnome.Showtime/x86_64/stable app/org.altlinux.Tuner/x86_64/stable app/com.github.Matoking.protontricks/x86_64/stable runtime/com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable runtime/com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable runtime/com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/25.08 app/io.missioncenter.MissionCenter/x86_64/stable
+flatpak remove -y --noninteractive app/com.github.Matoking.protontricks/x86_64/stable app/com.github.tchx84.Flatseal/x86_64/stable app/io.github.flattool.Warehouse/x86_64/stable app/com.vysp3r.ProtonPlus/x86_64/stable app/org.gnome.Calculator/x86_64/stable app/org.gnome.Calendar/x86_64/stable app/org.gnome.Characters/x86_64/stable app/org.gnome.Contacts/x86_64/stable app/org.gnome.Papers/x86_64/stable app/org.gnome.Loupe/x86_64/stable app/org.gnome.TextEditor/x86_64/stable app/org.gnome.Weather/x86_64/stable app/org.gnome.baobab/x86_64/stable app/org.gnome.clocks/x86_64/stable app/org.gnome.font-viewer/x86_64/stable app/org.gnome.Showtime/x86_64/stable app/org.altlinux.Tuner/x86_64/stable app/com.github.Matoking.protontricks/x86_64/stable runtime/com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable runtime/com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable runtime/com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/25.08 runtime/org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/25.08 app/io.missioncenter.MissionCenter/x86_64/stable
 
-# there's a bug with DistroShelf in that it may error when trying to uninstall, 
-# so i'm keeping it out of the above and attempting uninstall twice for posterity
-xargs -r flatpak remove -y --noninteractive app/com.ranfdev.DistroShelf/x86_64/stable
+# for some reason, Distroshelf sometimes doesn't uninstall correctly the first time
+flatpak remove -y --noninteractive app/com.ranfdev.DistroShelf/x86_64/stable
+flatpak remove -y --noninteractive app/com.ranfdev.DistroShelf/x86_64/stable
+
+# this removes gnome extension manager and extensions
+gsettings set org.gnome.shell disable-user-extensions true
+flatpack remove -y --noninteractive com.mattjakeman.ExtensionManager
+rm -rf /usr/share/gnome-shell/extensions ~/.local/share/gnome-shell/extensions
+
+# this cleans flatpak
+flatpak remove -y --noninteractive --unused
 
 # this removes dnf packages
 dnf5 remove -y firewall-config btrfs-assistant lutris 
@@ -40,8 +48,8 @@ rm -rf /etc/hhd
 rm -rf /usr/share/applications/hhd-ui /usr/share/applications/hhd-ui.desktop
 
 # this removes waydroid
-xargs -r waydroid session stop
-xargs -r waydroid container stop
+waydroid session stop
+waydroid container stop
 dnf5 remove -y waydroid
 rm -rf /var/lib/waydroid /home/.waydroid ~/waydroid ~/.share/waydroid ~/.local/share/applications/*aydroid* ~/.local/share/waydroid /usr/share/applications/Waydroind /usr/share/applications/*aydroid* /usr/libexec/*aydroid* /usr/lib/*aydroid*
 
@@ -52,13 +60,18 @@ rm -rf /var/lib/tailscale/tailscaled.state
 # this removes bbrew
 rm -rf /usr/share/applications/bbrew.desktop /usr/bin/bbrew-helper /usr/sbin/bbrew-helper
 
-# this removes the gnome extension manager and extensions
-xargs -r gsettings set org.gnome.shell disable-user-extensions true
-xargs -r flatpack remove -y --noninteractive com.mattjakeman.ExtensionManager
-rm -rf /usr/share/gnome-shell/extensions ~/.local/share/gnome-shell/extensions
-
 # this removes application icons if they're left over
 rm -rf  /usr/share/applications/btrf-assistant.desktop /usr/share/applications/com.gerbilsoft.rom-properties.rp-config.desktop
+
+# this cleans dnf
+dnf5 clean all
+
+### Configurations
+
+# this configures applications and environments
+# example: systemctl enable docker.service
+
+# this runs a configuration
 
 ### Scripts
 
@@ -66,6 +79,6 @@ rm -rf  /usr/share/applications/btrf-assistant.desktop /usr/share/applications/c
 # example: ujust setup tailscale
 
 # this runs ujust scripts
-xargs -r ujust setup-sunshine
-xargs -r ujust setup-decky
-xargs -r ujust --yes clean-system
+ujust setup-sunshine
+ujust setup-decky
+ujust --yes clean-system
